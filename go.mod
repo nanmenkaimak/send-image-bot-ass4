36d18d853c34576ee6ae699292e236dd6db05ed2
Ali Aristanov <aristanovali618@gmail.com>
2023-03-31 18:25:43 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/nanmenkaimak/send-image-bot-ass4
+
+go 1.19
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram/bot v0.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ mutex/main_mutex.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	bot "github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/nanmenkaimak/send-image-bot-ass4/keys"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+)
+
+type Photo struct {
+	ID   string `json:"id"`
+	URLs struct {
+		Regular string `json:"regular"`
+	} `json:"urls"`
+}
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	bot, err := bot.New(keys.Token(), bot.WithDefaultHandler(handler))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	var mutex sync.Mutex
+
+	if update.Message.Text == "image" || update.Message.Text == "/image" {
+		mutex.Lock()
+
+		photo, err := randomPhoto(keys.AccessKey())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		params := &bot.SendPhotoParams{
+			ChatID: update.Message.Chat.ID,
+			Photo:  &models.InputFileString{Data: photo.URLs.Regular},
+		}
+		if _, err := b.SendPhoto(ctx, params); err != nil {
+			log.Println(err)
+		}
+
+		mutex.Unlock()
+	}
+
+}
+
+func randomPhoto(accessKey string) (Photo, error) {
+	url := fmt.Sprintf("https://api.unsplash.com/photos/random?client_id=%s", accessKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return Photo{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Photo{}, err
+	}
+
+	var photos Photo
+	err = json.Unmarshal(body, &photos)
+	if err != nil {
+		return Photo{}, err
+	}
+
+	return photos, nil
+}
